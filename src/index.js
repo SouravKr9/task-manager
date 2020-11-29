13bedbc45b67fa58d8fd082a2a8f337901d322ee
SouravKr9 <souravkumarb175@gmail.com>
2020-11-29 21:47:37 +0530

    removing redundancies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const Task = require('./model/Task');
 const userRouter = require('./routers/userRouter');
 const taskRouter = require('./routers/taskRouter');
-const User = require('./model/User');
 require('./db/mongoose');
 
 const app = express();
-const port = process.env.PORT || PORT;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log('Server is running at port '+port);
